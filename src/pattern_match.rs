0ea6cb29d11dc8eeff3786178602bbaef1772803
Axel Lord <axel.lord2000@gmail.com>
2022-11-08 21:01:08 +0100

    added some commands and slit splitting

---
+++ src/pattern_match.rs
@@ -0,0 +1,22 @@
+use crate::token;
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::ops::Range;
+
+pub fn split_by_delim_to_ranges(list: &str) -> Vec<Range<usize>> {
+    lazy_static! {
+        static ref SPLIT_RE: Regex =
+            Regex::new(&[r#"\s"#, token::unsorted::TAG_DELIM, r#"\s|$"#].concat()).unwrap();
+    }
+
+    let mut next_start = 0;
+    SPLIT_RE
+        .find_iter(list)
+        .map(|m| {
+            let r = next_start..m.start();
+            next_start = m.end();
+            r
+        })
+        .filter(|r| !r.is_empty())
+        .collect()
+}
