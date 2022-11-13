c8c7e4b3c03f816645c408239d1ca0bf49e83c25
Axel Lord <axel.rinkaby@gmail.com>
2022-11-13 15:58:15 +0100

    made category storeable and split proc macros

--- library/src/category.rs
+++ library/src/category.rs
@@ -1,18 +1,33 @@
-use crate::{load::ParseErr, pattern_match, token, ContentString};
-use lazy_static::lazy_static;
-use regex::Regex;
+use crate::{token, ContentString};
+use bookmark_storage::Storeable;
 use std::{collections::HashMap, error::Error, ops::Range};
 
-#[derive(Debug)]
+#[derive(Debug, bookmark_derive::Storeable)]
 pub struct Category {
+    #[line]
     line: Option<ContentString>,
+
+    #[string]
+    #[token(token::category::ID)]
     id: Range<usize>,
+
+    #[string]
+    #[token(token::category::NAME)]
     name: Range<usize>,
+
+    #[string]
+    #[token(token::category::DESCRIPTION)]
     description: Range<usize>,
-    identifier: Range<usize>,
+
+    #[composite(identifier)]
+    #[token(token::category::IDENTIFIER)]
     identifiers: Vec<Range<usize>>,
-    subcategory: Range<usize>,
+    identifier: Range<usize>,
+
+    #[composite(subcategory)]
+    #[token(token::category::SUBCATEGORY)]
     subcategories: Vec<Range<usize>>,
+    subcategory: Range<usize>,
 }
 
 #[derive(Clone, Debug)]
@@ -43,126 +58,7 @@ impl<'a> IdentifierContainer<'a> {
     }
 }
 
-impl Clone for Category {
-    fn clone(&self) -> Self {
-        Self::with_string(self.to_line(), None).unwrap()
-    }
-}
-impl From<Category> for String {
-    fn from(c: Category) -> Self {
-        c.to_line()
-    }
-}
-
 impl Category {
-    pub fn new<'a>(
-        id: &str,
-        name: &str,
-        description: &str,
-        identifiers: impl Iterator<Item = &'a str>,
-        subcategories: impl Iterator<Item = &'a str>,
-    ) -> Self {
-        Self::with_string(
-            Self::create_line(id, name, description, identifiers, subcategories),
-            None,
-        )
-        .unwrap()
-    }
-
-    pub fn with_str(line: &str, line_num: Option<usize>) -> Result<Self, ParseErr> {
-        Self::with_string(line.into(), line_num)
-    }
-
-    pub fn with_string(line: String, line_num: Option<usize>) -> Result<Self, ParseErr> {
-        lazy_static! {
-            static ref LINE_RE: Regex = Regex::new(
-                &[
-                    r"^",
-                    token::category::ID,
-                    pattern_match::WHITESPACE_PADDED_GROUP,
-                    token::category::NAME,
-                    pattern_match::WHITESPACE_PADDED_GROUP,
-                    token::category::DESCRIPTION,
-                    pattern_match::WHITESPACE_PADDED_GROUP,
-                    token::category::IDENTIFIER,
-                    pattern_match::WHITESPACE_PADDED_GROUP,
-                    token::category::SUBCATEGORY,
-                    pattern_match::WHITESPACE_PADDED_GROUP,
-                    r"$"
-                ]
-                .concat()
-            )
-            .unwrap();
-        }
-
-        let err = || ParseErr::Line(Some(line.clone()), line_num);
-
-        let captures = LINE_RE.captures(&line).ok_or_else(err)?;
-
-        let id = captures.get(1).ok_or_else(err)?.range();
-        let name = captures.get(2).ok_or_else(err)?.range();
-        let description = captures.get(3).ok_or_else(err)?.range();
-
-        let identifier = captures.get(4).ok_or_else(err)?.range();
-        let identifiers = pattern_match::split_by_delim_to_ranges(&line[identifier.clone()]);
-
-        let subcategory = captures.get(5).ok_or_else(err)?.range();
-        let subcategories = pattern_match::split_by_delim_to_ranges(&line[subcategory.clone()]);
-
-        Ok(Self {
-            line: Some(ContentString::UnappendedTo(line)),
-            id,
-            name,
-            description,
-            identifier,
-            identifiers,
-            subcategory,
-            subcategories,
-        })
-    }
-
-    pub fn to_line(&self) -> String {
-        if let Some(ContentString::UnappendedTo(line)) = self.line.as_ref() {
-            line.clone()
-        } else {
-            Self::create_line(
-                self.id(),
-                self.name(),
-                self.description(),
-                self.identifiers(),
-                self.subcategories(),
-            )
-        }
-    }
-
-    pub fn is_edited(&self) -> bool {
-        self.line.as_ref().unwrap().is_appended_to()
-    }
-
-    pub fn id(&self) -> &str {
-        &self.raw_line()[self.id.clone()]
-    }
-
-    pub fn name(&self) -> &str {
-        &self.raw_line()[self.name.clone()]
-    }
-
-    pub fn description(&self) -> &str {
-        &self.raw_line()[self.description.clone()]
-    }
-
-    pub fn identifiers(&self) -> impl Iterator<Item = &str> {
-        self.identifiers
-            .iter()
-            .map(|r| &self.raw_line()[self.identifier.clone()][r.clone()])
-    }
-
-    pub fn subcategories(&self) -> impl Iterator<Item = &str> {
-        self.subcategories
-            .iter()
-            .map(|r| &self.raw_line()[self.subcategory.clone()][r.clone()])
-    }
-
     pub fn identifier_container<'a>(&'a self) -> Result<IdentifierContainer<'a>, IdentifierErr> {
         let mut identifier_container: IdentifierContainer<'a> = Default::default();
 
@@ -197,45 +93,6 @@ impl Category {
         }
 
         Ok(identifier_container)
-    }
-
-    pub fn subcategory_vec<'a>(
-        &self,
-        categories: impl Clone + Iterator<Item = &'a Category>,
-    ) -> Vec<&'a Category> {
-        self.subcategories()
-            .filter_map(|subcat| categories.clone().find(|cat| cat.id() == subcat))
-            .collect()
-    }
-
-    fn create_line<'a>(
-        id: &str,
-        name: &str,
-        description: &str,
-        identifiers: impl Iterator<Item = &'a str>,
-        subcategories: impl Iterator<Item = &'a str>,
-    ) -> String {
-        format!(
-            "{} {} {} {} {} {} {} {} {} {}",
-            token::category::ID,
-            id,
-            token::category::NAME,
-            name,
-            token::category::DESCRIPTION,
-            description,
-            token::category::IDENTIFIER,
-            identifiers
-                .collect::<Vec<&str>>()
-                .join(&[" ", token::DELIM, " "].concat()),
-            token::category::SUBCATEGORY,
-            subcategories
-                .collect::<Vec<&str>>()
-                .join(&[" ", token::DELIM, " "].concat()),
-        )
-    }
-
-    fn raw_line(&self) -> &str {
-        self.line.as_ref().unwrap().ref_any()
     }
 }
 
