6585e28f0b3061893e39f2b625dedfdcab9c5d7d
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2022-11-14 00:22:48 +0100

    simplified save functionality to rely on traits

--- storage/src/load.rs
+++ storage/src/load.rs
@@ -1,20 +1,17 @@
 use crate::{Listed, ParseErr};
-use std::{fs::File, io};
+use std::io::{self, Read};
 
-pub fn load<T>(path: &str) -> Result<Vec<T>, ParseErr>
+pub fn load<T, R>(reader: &mut R) -> Result<Vec<T>, ParseErr>
 where
     T: Listed,
+    R: Read,
 {
-    io::read_to_string(
-        File::open(path)
-            .map_err(|err| ParseErr::Other(format!("could not open {}: {}", path, err)))?,
-    )
-    .map_err(|err| ParseErr::Other(format!("failed to read {}: {}", path, err)))?
-    .lines()
-    .enumerate()
-    .skip_while(|(_, l)| !l.contains(T::token_begin()))
-    .skip(1)
-    .take_while(|(_, l)| !l.contains(T::token_end()))
-    .map(|(i, l)| T::with_str(l, Some(i)))
-    .collect()
+    io::read_to_string(reader)?
+        .lines()
+        .enumerate()
+        .skip_while(|(_, l)| !l.contains(T::token_begin()))
+        .skip(1)
+        .take_while(|(_, l)| !l.contains(T::token_end()))
+        .map(|(i, l)| T::with_str(l, Some(i)))
+        .collect()
 }
