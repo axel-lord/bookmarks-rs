92c3cadc3f5788357512132de3723bd8da1ebab9
Axel Lord <axel.lord2000@gmail.com>
2022-11-14 16:24:21 +0100

    improved category list

---
+++ library/src/command/list.rs
@@ -0,0 +1,15 @@
+use crate::command_map::CommandErr;
+
+pub fn wrap_if_negative(number: isize, max: usize) -> Result<usize, CommandErr> {
+    if number.abs() as usize > max {
+        return Err(CommandErr::Execution(format!(
+            "number {number} larger than max value {max}"
+        )));
+    }
+
+    Ok(if number >= 0 {
+        number as usize
+    } else {
+        max - number.abs() as usize
+    })
+}
