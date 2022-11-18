eb66720e1ed1cf066932d12f880862ed0a8afbe3
Axel Lord <axel.lord2000@gmail.com>
2022-11-18 17:34:54 +0100

    made save generic

--- library/src/reset.rs
+++ library/src/reset.rs
@@ -4,6 +4,8 @@ use crate::shared;
 pub struct ResetValues {
     pub bookmark_buffer: shared::Buffer,
     pub bookmarks: shared::Bookmarks,
+    pub category_buffer: shared::Buffer,
+    pub categories: shared::Categroies,
     pub selected_bookmark: shared::Selected,
     pub selected_category: shared::Selected,
 }
@@ -11,6 +13,7 @@ pub struct ResetValues {
 impl ResetValues {
     pub fn reset(&self) {
         self.bookmark_buffer.reset(&self.bookmarks);
+        self.category_buffer.reset(&self.categories);
         self.selected_bookmark.clear();
         self.selected_category.clear();
     }
