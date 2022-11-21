fe6cd6be53a5ed822d60021f45493398770e046f
Axel Lord <axel.rinkaby@gmail.com>
2022-11-21 01:51:13 +0100

    added a generic impl of print

---
+++ library/src/command/print.rs
@@ -0,0 +1,32 @@
+use crate::{
+    command::{Command, CommandErr},
+    shared,
+};
+
+use bookmark_storage::Storeable;
+
+pub fn build<T>(storage: shared::Storage<T>, selected: shared::Selected) -> Box<dyn Command>
+where
+    T: 'static + Storeable + std::fmt::Display,
+{
+    Box::new(move |args: &[_]| {
+        if args.len() != 0 {
+            return Err(CommandErr::Usage(
+                "print should be called without any arguments".into(),
+            ));
+        }
+        if selected.is_empty() {
+            return Err(CommandErr::Execution("noting selected".into()));
+        }
+
+        let bookmarks = storage.borrow();
+
+        let Some(selected_item) = selected.get(&bookmarks) else {
+            return Err(CommandErr::Execution("selected item does not exist".into()));
+        };
+
+        println!("{}. {:#}", selected.index().unwrap(), selected_item);
+
+        Ok(())
+    })
+}
