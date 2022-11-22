acbf820cdf48c416ab331b23788884be79510147
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2022-11-22 12:03:51 +0100

    added unique

---
+++ library/src/command/bookmark/unique.rs
@@ -0,0 +1,33 @@
+use crate::{
+    command::{Command, CommandErr},
+    reset::ResetValues,
+    shared,
+};
+
+pub fn build(bookmarks: shared::Bookmarks, reset_values: ResetValues) -> Box<dyn Command> {
+    Box::new(move |args: &[_]| {
+        if args.len() != 0 {
+            return Err(CommandErr::Usage(
+                "unique should be called without any arguments".into(),
+            ));
+        }
+
+        let mut bookmarks = bookmarks.borrow_mut();
+
+        let mut removed_count = 0usize;
+        bookmarks.sort_by(|a, b| a.url().partial_cmp(b.url()).unwrap());
+        bookmarks.dedup_by(|a, b| {
+            if a.url().eq_ignore_ascii_case(b.url()) {
+                removed_count += 1;
+                true
+            } else {
+                false
+            }
+        });
+        reset_values.reset();
+
+        println!("remvoved {removed_count} bookmarks");
+
+        Ok(())
+    })
+}
