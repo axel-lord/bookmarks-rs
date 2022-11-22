bd68f040ed3876466fc30fa3e7b8f1107ac20a4d
Axel Lord <axel.lord2000@gmail.com>
2022-11-22 18:00:46 +0100

    find substring in string

--- storage/src/pattern_match.rs
+++ storage/src/pattern_match.rs
@@ -1,43 +1,114 @@
 use crate::{token, Field};
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::ops::Range;
+use std::{convert::TryInto, ops::Range};
 
 pub const WHITESPACE_PADDED_GROUP: &str = r"\s*(.*?)\s*";
 
 pub fn split_by_delim_to_ranges(list: &str) -> Vec<Range<usize>> {
-    lazy_static! {
-        static ref SPLIT_RE: Regex =
-            Regex::new(&[r#"\s"#, token::DELIM, r#"\s|$"#].concat()).unwrap();
-    }
-
-    let mut next_start = 0;
-    SPLIT_RE
-        .find_iter(list)
-        .map(move |m| {
-            let r = next_start..m.start();
-            next_start = m.end();
-            r
+    list.split(token::DELIM)
+        .filter_map(|item| {
+            let trimmed = item.trim();
+            if trimmed.is_empty() {
+                None
+            } else {
+                Some(substring_location(list, trimmed).unwrap())
+            }
         })
-        .filter(|r| !r.is_empty())
         .collect()
 }
 
 pub fn split_list_field<'a>(list_field: &'a str) -> impl 'a + Iterator<Item = Field> {
-    lazy_static! {
-        static ref SPLIT_RE: Regex =
-            Regex::new(&[r#"\s"#, token::DELIM, r#"\s|$"#].concat()).unwrap();
-    }
-
-    let mut next_start = 0;
-    SPLIT_RE.find_iter(list_field).filter_map(move |m| {
-        let range = next_start..m.start();
-        next_start = m.end();
-
-        if range.is_empty() {
+    list_field.split(token::DELIM).filter_map(|item| {
+        let trimmed = item.trim();
+        if trimmed.is_empty() {
             None
         } else {
-            Some(range.into())
+            Some(substring_location(list_field, trimmed).unwrap().into())
         }
     })
+}
+
+pub fn substring_location(string: &str, substring: &str) -> Option<Range<usize>> {
+    let string_ptr = string.as_ptr();
+    let substring_ptr = substring.as_ptr();
+
+    if substring_ptr < string_ptr
+        || unsafe {
+            substring_ptr.offset(substring.len().try_into().ok()?)
+                > string_ptr.offset(string.len().try_into().ok()?)
+        }
+    {
+        return None;
+    }
+
+    let start = unsafe { substring_ptr.offset_from(string_ptr) } as usize;
+    let end = unsafe {
+        substring_ptr
+            .offset(substring.len().try_into().ok()?)
+            .offset_from(string_ptr)
+    } as usize;
+
+    Some(start..end)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn substring_location_basic() {
+        let string = "Hello there! Nice to meet you!";
+        assert_eq!(substring_location(&string, &string[0..5]), Some(0..5));
+        assert_eq!(substring_location(&string, &string[2..5]), Some(2..5));
+        assert_eq!(substring_location(&string, &string[2..7]), Some(2..7));
+    }
+
+    #[test]
+    pub fn field_split_basic() {
+        let field = "a <,> b <,> c";
+
+        assert_eq!(
+            split_by_delim_to_ranges(field)
+                .iter()
+                .map(|r| &field[r.clone()])
+                .collect::<Vec<_>>(),
+            vec!["a", "b", "c"]
+        );
+        assert_eq!(
+            split_list_field(field)
+                .map(|f| &field[f.0.clone()])
+                .collect::<Vec<_>>(),
+            vec!["a", "b", "c"]
+        );
+        let field = "a <,> b <,> c <,>";
+
+        assert_eq!(
+            split_by_delim_to_ranges(field)
+                .iter()
+                .map(|r| &field[r.clone()])
+                .collect::<Vec<_>>(),
+            vec!["a", "b", "c"]
+        );
+        assert_eq!(
+            split_list_field(field)
+                .map(|f| &field[f.0.clone()])
+                .collect::<Vec<_>>(),
+            vec!["a", "b", "c"]
+        );
+
+        let field = "";
+
+        assert_eq!(
+            split_by_delim_to_ranges(field)
+                .iter()
+                .map(|r| &field[r.clone()])
+                .collect::<Vec<_>>(),
+            Vec::<&str>::new()
+        );
+        assert_eq!(
+            split_list_field(field)
+                .map(|f| &field[f.0.clone()])
+                .collect::<Vec<_>>(),
+            Vec::<&str>::new()
+        );
+    }
 }
