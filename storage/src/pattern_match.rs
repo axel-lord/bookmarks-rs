f4ec8a8ef6b273fdd7409d5f89830b397338ff3e
Axel Lord <axel.rinkaby@gmail.com>
2022-11-26 20:32:37 +0100

    added some documentation

--- storage/src/pattern_match.rs
+++ storage/src/pattern_match.rs
@@ -1,8 +1,10 @@
 use crate::{token, Field};
 use std::{convert::TryInto, ops::Range};
 
-pub const WHITESPACE_PADDED_GROUP: &str = r"\s*(.*?)\s*";
-
+/// Split a string slice into a vector of string slices with the delimiter [token::DELIM].
+///
+/// # Panics
+/// If [substring_location] fails, should in this case never happen.
 pub fn split_by_delim_to_ranges(list: &str) -> Vec<Range<usize>> {
     list.split(token::DELIM)
         .filter_map(|item| {
@@ -16,6 +18,10 @@ pub fn split_by_delim_to_ranges(list: &str) -> Vec<Range<usize>> {
         .collect()
 }
 
+/// Split a string slice into an iterator of [Field] based on the delimiter [token::DELIM].
+///
+/// # Panics
+/// If [substring_location] fails, should in this case never happen.
 pub fn split_list_field(list_field: &'_ str) -> impl '_ + Iterator<Item = Field> {
     list_field.split(token::DELIM).filter_map(|item| {
         let trimmed = item.trim();
@@ -27,6 +33,11 @@ pub fn split_list_field(list_field: &'_ str) -> impl '_ + Iterator<Item = Field>
     })
 }
 
+/// Get the location of a string slice in another string slice.
+///
+/// The position is by pointer offset and as such while O(n) only works if the
+/// slice is a subslice. Some meassures have been taken to detect invalid use
+/// and return None in such a case, however no guarantees are given.
 pub fn substring_location(string: &str, substring: &str) -> Option<Range<usize>> {
     let string_ptr = string.as_ptr();
     let substring_ptr = substring.as_ptr();
--- storage/src/save.rs
+++ storage/src/save.rs
@@ -2,11 +2,13 @@ use std::io::{self, Write};
 
 use crate::Listed;
 
-pub fn save<'a, T, I, W>(writer: &mut W, content: I) -> io::Result<()>
+/// Write the items of an iterator of [Listed].
+///
+/// # Errors
+/// If a write fails.
+pub fn save<'a, T>(writer: &mut impl Write, content: impl Iterator<Item = &'a T>) -> io::Result<()>
 where
     T: 'a + Listed,
-    I: Iterator<Item = &'a T>,
-    W: Write,
 {
     writeln!(writer, "{}", T::TOKEN_BEGIN)?;
 
