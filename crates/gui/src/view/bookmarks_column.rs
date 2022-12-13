aad2b9ff680e55d3c0fa76b6574e64db6de314fd
Axel Lord <axel.rinkaby@gmail.com>
2022-12-13 22:20:39 +0100

    Split rows of categories and bookmarks to separate functions

--- crates/gui/src/view/bookmarks_column.rs
+++ crates/gui/src/view/bookmarks_column.rs
@@ -1,10 +1,27 @@
 use std::ops::Range;
 
 use crate::Msg;
+use bookmark_library::Bookmark;
 use iced::{
-    widget::{button, column, row, scrollable, text, Column},
+    widget::{button, column, row, scrollable, text, Column, Row},
     Length,
 };
+
+fn bookmark_row<'a, Renderer>(index: usize, bookmark: &Bookmark) -> Row<'a, Msg, Renderer>
+where
+    Renderer: 'a + iced_native::text::Renderer,
+    <Renderer as iced_native::Renderer>::Theme: button::StyleSheet + text::StyleSheet,
+{
+    row![
+        button("Goto")
+            .on_press(Msg::GotoBookmarkLocation(index))
+            .width(Length::Shrink),
+        text(bookmark.description()).width(Length::Fill),
+        text(bookmark.url()).width(Length::Fill),
+    ]
+    .spacing(3)
+    .align_items(iced::Alignment::Center)
+}
 
 pub fn bookmark_column<'a, Renderer>(
     bookmarks: impl IntoIterator<Item = (usize, impl AsRef<bookmark_library::Bookmark>)>,
@@ -23,17 +40,7 @@ where
                 .skip(bookmark_range.start)
                 .take(bookmark_range.count())
                 .fold(Column::new(), |r, (i, b)| {
-                    r.push(
-                        row![
-                            button("Goto")
-                                .on_press(Msg::GotoBookmarkLocation(i))
-                                .width(Length::Shrink),
-                            text(b.as_ref().description()).width(Length::Fill),
-                            text(b.as_ref().url()).width(Length::Fill)
-                        ]
-                        .align_items(iced::Alignment::Center)
-                        .spacing(3),
-                    )
+                    r.push(bookmark_row(i, b.as_ref()))
                 })
                 .spacing(3)
         )
--- crates/gui/src/view/category_column.rs
+++ crates/gui/src/view/category_column.rs
@@ -1,5 +1,19 @@
 use crate::Msg;
-use iced::widget::{button, column, row, scrollable, text, Column};
+use bookmark_library::Category;
+use iced::widget::{button, column, row, scrollable, text, Column, Row};
+
+fn category_row<'a, Renderer>(index: usize, category: &Category) -> Row<'a, Msg, Renderer>
+where
+    Renderer: 'a + iced_native::text::Renderer,
+    <Renderer as iced_native::Renderer>::Theme: text::StyleSheet + button::StyleSheet,
+{
+    row![
+        button("Apply").on_press(Msg::ApplyCategory(index)),
+        text(category.name()),
+    ]
+    .spacing(3)
+    .align_items(iced::Alignment::Center)
+}
 
 pub fn category_column<'a, Renderer>(
     categories: impl IntoIterator<Item = (usize, impl AsRef<bookmark_library::Category>)>,
@@ -15,14 +29,7 @@ where
             categories
                 .into_iter()
                 .fold(Column::new(), |r, (i, c)| {
-                    r.push(
-                        row![
-                            button("Apply").on_press(Msg::ApplyCategory(i)),
-                            text(c.as_ref().name().to_string()),
-                        ]
-                        .spacing(3)
-                        .align_items(iced::Alignment::Center),
-                    )
+                    r.push(category_row(i, c.as_ref()))
                 })
                 .spacing(3)
         )
