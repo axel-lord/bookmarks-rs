a97707450c6645c879ff7abc0411ab07c6806e8b
Axel Lord <axel.rinkaby@gmail.com>
2022-12-13 01:25:31 +0100

    Added functionality to change how many bookmarsk are shown

--- crates/gui/src/view.rs
+++ crates/gui/src/view.rs
@@ -1,34 +1,45 @@
 mod bookmarks_column;
 mod category_column;
 
+use std::ops::Range;
+
 use crate::Msg;
 use bookmark_library::{container::BufferStorage, Bookmark, Category};
 use bookmarks_column::bookmark_column;
 use category_column::category_column;
 use iced::{
     widget::{
-        button, column, horizontal_rule, horizontal_space, row, rule, scrollable, text,
+        button, column, horizontal_rule, horizontal_space, row, rule, scrollable, text, text_input,
         vertical_rule, Column, Row,
     },
     Length,
 };
-use iced_native::alignment::Vertical;
 
-fn tool_row<'a, Renderer>(status: &str) -> Row<'a, Msg, Renderer>
+fn tool_row<'a, Renderer>(status: &str, shown_bookmarks: &str) -> Row<'a, Msg, Renderer>
 where
     Renderer: 'a + iced_native::text::Renderer,
-    <Renderer as iced_native::Renderer>::Theme: text::StyleSheet + button::StyleSheet,
+    <Renderer as iced_native::Renderer>::Theme:
+        text::StyleSheet + button::StyleSheet + text_input::StyleSheet,
 {
     row![
         button("Reset").on_press(Msg::Reset),
+        text("Shown bookmarks:"),
+        text_input("...", shown_bookmarks, |s| Msg::ShownBookmarksChanged(
+            s.into()
+        ))
+        .width(Length::Units(50)),
         horizontal_space(Length::Fill),
-        text(status).vertical_alignment(Vertical::Center),
+        text(status),
     ]
+    .align_items(iced::Alignment::Center)
+    .spacing(3)
+    .padding(3)
 }
 
 fn content_row<'a, Renderer>(
     bookmarks: &BufferStorage<Bookmark>,
     categories: &BufferStorage<Category>,
+    bookmark_range: Range<usize>,
 ) -> Row<'a, Msg, Renderer>
 where
     Renderer: 'a + iced_native::text::Renderer,
@@ -38,23 +49,29 @@ where
     row![
         scrollable(category_column(categories.iter_indexed()).width(Length::Shrink)),
         vertical_rule(3),
-        scrollable(bookmark_column(bookmarks.iter_indexed()).width(Length::Fill))
+        scrollable(bookmark_column(bookmarks.iter_indexed(), bookmark_range).width(Length::Fill))
     ]
+    .align_items(iced::Alignment::Start)
 }
 
 pub fn application_view<'a, Renderer>(
     bookmarks: &BufferStorage<Bookmark>,
     categories: &BufferStorage<Category>,
     status: &str,
+    shown_bookmarks: &str,
+    bookmark_range: Range<usize>,
 ) -> Column<'a, Msg, Renderer>
 where
     Renderer: 'a + iced_native::text::Renderer,
-    <Renderer as iced_native::Renderer>::Theme:
-        text::StyleSheet + button::StyleSheet + rule::StyleSheet + scrollable::StyleSheet,
+    <Renderer as iced_native::Renderer>::Theme: text::StyleSheet
+        + button::StyleSheet
+        + rule::StyleSheet
+        + scrollable::StyleSheet
+        + text_input::StyleSheet,
 {
     column![
-        tool_row(status),
+        tool_row(status, shown_bookmarks),
         horizontal_rule(3),
-        content_row(bookmarks, categories)
+        content_row(bookmarks, categories, bookmark_range)
     ]
 }
