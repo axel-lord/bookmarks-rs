c20bd92ad1c329d99389d1e4604773c222637a7b
Axel Lord <axel.rinkaby@gmail.com>
2022-12-13 20:58:04 +0100

    Titles no longer scroll away

--- crates/gui/src/view/bookmarks_column.rs
+++ crates/gui/src/view/bookmarks_column.rs
@@ -2,7 +2,7 @@ use std::ops::Range;
 
 use crate::Msg;
 use iced::{
-    widget::{button, row, rule, text, Column},
+    widget::{button, column, row, scrollable, text, Column},
     Length,
 };
 
@@ -13,25 +13,31 @@ pub fn bookmark_column<'a, Renderer>(
 where
     Renderer: 'a + iced_native::text::Renderer,
     <Renderer as iced_native::Renderer>::Theme:
-        text::StyleSheet + button::StyleSheet + rule::StyleSheet,
+        text::StyleSheet + button::StyleSheet + scrollable::StyleSheet,
 {
-    bookmarks
-        .into_iter()
-        .skip(bookmark_range.start)
-        .take(bookmark_range.count())
-        .fold(Column::new().push(text("Bookmarks:")), |r, (i, b)| {
-            r.push(
-                row![
-                    button("Goto")
-                        .on_press(Msg::BookmarkClicked(i))
-                        .width(Length::Shrink),
-                    text(b.as_ref().description()).width(Length::Fill),
-                    text(b.as_ref().url()).width(Length::Fill)
-                ]
-                .align_items(iced::Alignment::Center)
-                .spacing(3),
-            )
-        })
-        .spacing(3)
-        .padding(3)
+    column![
+        text("Bookmarks:"),
+        scrollable(
+            bookmarks
+                .into_iter()
+                .skip(bookmark_range.start)
+                .take(bookmark_range.count())
+                .fold(Column::new(), |r, (i, b)| {
+                    r.push(
+                        row![
+                            button("Goto")
+                                .on_press(Msg::BookmarkClicked(i))
+                                .width(Length::Shrink),
+                            text(b.as_ref().description()).width(Length::Fill),
+                            text(b.as_ref().url()).width(Length::Fill)
+                        ]
+                        .align_items(iced::Alignment::Center)
+                        .spacing(3),
+                    )
+                })
+                .spacing(3)
+        )
+    ]
+    .padding(3)
+    .spacing(3)
 }
