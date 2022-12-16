68cf4430274da423d8e40e724cbd67db686426b7
Axel Lord <axel.lord2000@gmail.com>
2022-12-17 00:23:26 +0100

    Fully Added max width for bookmarks and info

--- crates/gui/src/view/bookmarks_column.rs
+++ crates/gui/src/view/bookmarks_column.rs
@@ -6,8 +6,14 @@ use iced::{
     widget::{button, column, row, scrollable, text, Column, Row},
     Length,
 };
+use unicode_segmentation::UnicodeSegmentation;
 
-fn bookmark_row<'a, Renderer>(index: usize, bookmark: &Bookmark) -> Row<'a, Msg, Renderer>
+fn bookmark_row<'a, Renderer>(
+    index: usize,
+    url_width: Option<usize>,
+    desc_width: Option<usize>,
+    bookmark: &Bookmark,
+) -> Row<'a, Msg, Renderer>
 where
     Renderer: 'a + iced_native::text::Renderer,
     <Renderer as iced_native::Renderer>::Theme: button::StyleSheet + text::StyleSheet,
@@ -16,8 +22,44 @@ where
         button("Goto")
             .on_press(Msg::GotoBookmarkLocation(index))
             .width(Length::Shrink),
-        text(bookmark.description()).width(Length::Fill),
-        text(bookmark.url()).width(Length::Fill),
+        text(
+            desc_width
+                .map(|w| {
+                    let val = bookmark.description();
+                    let letters = val
+                        .grapheme_indices(true)
+                        .take(w + 1)
+                        .map(|(i, _)| i)
+                        .collect::<Vec<_>>();
+
+                    if letters.len() == w + 1 {
+                        format!("{}...", &val[0..letters[w.saturating_sub(3)]])
+                    } else {
+                        val.to_string()
+                    }
+                })
+                .unwrap_or_else(|| bookmark.description().to_string())
+        )
+        .width(Length::Fill),
+        text(
+            url_width
+                .map(|w| {
+                    let val = bookmark.url();
+                    let letters = val
+                        .grapheme_indices(true)
+                        .take(w + 1)
+                        .map(|(i, _)| i)
+                        .collect::<Vec<_>>();
+
+                    if letters.len() == w + 1 {
+                        format!("{}...", &val[0..letters[w.saturating_sub(3)]])
+                    } else {
+                        val.to_string()
+                    }
+                })
+                .unwrap_or_else(|| bookmark.url().to_string())
+        )
+        .width(Length::Fill),
     ]
     .spacing(3)
     .align_items(iced::Alignment::Center)
@@ -26,6 +68,8 @@ where
 pub fn bookmark_column<'a, Renderer>(
     bookmarks: impl IntoIterator<Item = (usize, impl AsRef<bookmark_library::Bookmark>)>,
     bookmark_range: Range<usize>,
+    url_width: Option<usize>,
+    desc_width: Option<usize>,
 ) -> Column<'a, Msg, Renderer>
 where
     Renderer: 'a + iced_native::text::Renderer,
@@ -40,7 +84,7 @@ where
                 .skip(bookmark_range.start)
                 .take(bookmark_range.count())
                 .fold(Column::new(), |r, (i, b)| {
-                    r.push(bookmark_row(i, b.as_ref()))
+                    r.push(bookmark_row(i, url_width, desc_width, b.as_ref()))
                 })
                 .spacing(3)
         )
