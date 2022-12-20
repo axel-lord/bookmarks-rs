a39a9f401d64ba97e2dadbcd4b2ed618e42fe0b6
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2022-12-20 02:00:58 +0100

    Started nesting of cats

--- crates/gui/src/app/view/category_column.rs
+++ crates/gui/src/app/view/category_column.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::{AppView, Msg};
 use bookmark_library::Category;
 use iced::widget::{button, column, row, scrollable, text, Column, Row};
@@ -21,13 +23,54 @@ where
     <Renderer as iced_native::Renderer>::Theme:
         text::StyleSheet + button::StyleSheet + scrollable::StyleSheet,
 {
+    let cat_map = app_view
+        .categories
+        .storage
+        .iter()
+        .enumerate()
+        .map(|(i, c)| (<Box<str>>::from(c.id()), i))
+        .collect::<HashMap<_, _>>();
+
+    let mut cat_stack = app_view
+        .infos
+        .storage
+        .iter()
+        .flat_map(|i| {
+            i.categories()
+                .collect::<Vec<_>>()
+                .into_iter()
+                .rev()
+                .map(|id| (0usize, <Box<str>>::from(id)))
+        })
+        .collect::<Vec<_>>();
+
+    let mut cat_iter = Vec::new();
+    while !cat_stack.is_empty() {
+        let (level, cat_id) = cat_stack.pop().unwrap();
+
+        if let Some(i) = cat_map.get(&cat_id) {
+            let cat = &app_view.categories.storage[*i];
+
+            cat_stack.extend(
+                cat.subcategories()
+                    .collect::<Vec<_>>()
+                    .into_iter()
+                    .rev()
+                    .map(|id| (level + 1, <Box<str>>::from(id))),
+            );
+
+            cat_iter.push((*i, level, cat))
+        }
+    }
+
     column![
         text("Categories: "),
         scrollable(
-            app_view
-                .categories
-                .iter_indexed()
-                .fold(Column::new(), |r, (i, c)| { r.push(category_row(i, c)) })
+            cat_iter
+                .into_iter()
+                .fold(Column::new(), |r, (i, _l, c)| {
+                    r.push(category_row(i, c))
+                })
                 .spacing(3)
         )
     ]
