d7852ce773b50a0f399cdf76d03e78f235299934
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2023-01-08 20:52:45 +0100

    Added more stats

--- crates/gui/src/app/log_pane.rs
+++ crates/gui/src/app/log_pane.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, sync::mpsc, thread};
+use std::{collections::HashMap, fmt::Display, sync::mpsc, thread};
 
 use bookmark_library::{shared, Bookmark};
 use conv::prelude::*;
@@ -15,6 +15,36 @@ use crate::{Msg, View};
 
 use super::ChannelMessage;
 
+#[derive(Clone, Debug, Default)]
+pub struct UrlMap {
+    occurance_count: HashMap<String, usize>,
+    parse_issues: Vec<(Bookmark, url::ParseError)>,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct UrlMapTally {
+    parse_issues: usize,
+    no_domain: usize,
+    domains: usize,
+}
+
+impl UrlMap {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn tally(&self) -> UrlMapTally {
+        let no_domain = self.occurance_count.get("<no domain>").copied();
+        let domain_count = self.occurance_count.keys().len() - usize::from(no_domain.is_some());
+
+        UrlMapTally {
+            parse_issues: self.parse_issues.len(),
+            no_domain: no_domain.unwrap_or(0),
+            domains: domain_count,
+        }
+    }
+}
+
 #[derive(Clone, Copy, Default, Debug)]
 pub enum LogPane {
     #[default]
@@ -25,18 +55,21 @@ pub enum LogPane {
 #[derive(Clone, Copy, Debug)]
 pub enum Metric {
     AverageContentStringLength,
+    UrlOccurances,
 }
 
-#[derive(Clone, Copy, Debug, Default)]
+#[derive(Clone, Debug, Default)]
 pub enum MetricValue {
     #[default]
     None,
     Float(f64),
+    UrlMap(UrlMap),
 }
 
-#[derive(Clone, Default, Debug, Copy)]
+#[derive(Clone, Default, Debug)]
 pub struct Metrics {
     average_content_string_length: MetricValue,
+    url_occurances: MetricValue,
 }
 
 pub trait IntoMetricValue {
@@ -87,20 +120,23 @@ impl Display for MetricValue {
         match self {
             MetricValue::None => write!(f, "None"),
             MetricValue::Float(v) => write!(f, "{v}"),
+            MetricValue::UrlMap(v) => write!(f, "{:?}", v.tally()),
         }
     }
 }
 
 impl Metrics {
-    pub fn get(&self, metric: Metric) -> MetricValue {
+    pub fn get(&self, metric: Metric) -> &MetricValue {
         match metric {
-            Metric::AverageContentStringLength => self.average_content_string_length,
+            Metric::AverageContentStringLength => &self.average_content_string_length,
+            Metric::UrlOccurances => &self.url_occurances,
         }
     }
 
     pub fn set(&mut self, metric: Metric, value: MetricValue) {
         match metric {
             Metric::AverageContentStringLength => self.average_content_string_length = value,
+            Metric::UrlOccurances => self.url_occurances = value,
         }
     }
 
@@ -130,6 +166,37 @@ impl Metrics {
             ))
         });
     }
+
+    pub fn gather_url_occurances(
+        tx: mpsc::Sender<ChannelMessage>,
+        bookmarks: shared::BufferStorage<Bookmark>,
+    ) {
+        thread::spawn(move || {
+            let bookmarks = bookmarks.read().expect("posioned lock");
+            tx.send(ChannelMessage::GatheredMetric(Metric::UrlOccurances, {
+                let mut map = UrlMap::new();
+
+                for bookmark in bookmarks.storage.iter() {
+                    let url = match url::Url::parse(bookmark.url()) {
+                        Ok(url) => url,
+                        Err(err) => {
+                            map.parse_issues.push((bookmark.clone(), err));
+                            continue;
+                        }
+                    };
+
+                    let entry = map
+                        .occurance_count
+                        .entry(String::from(url.domain().unwrap_or("<no domain>")))
+                        .or_insert(0);
+
+                    *entry += 1;
+                }
+
+                MetricValue::UrlMap(map)
+            }))
+        });
+    }
 }
 
 impl LogPane {
@@ -156,7 +223,8 @@ impl LogPane {
             LogPane::Stats => {
                 let content_string_length = row![
                     button("Gather")
-                        .on_press(Msg::GatherMetric(Metric::AverageContentStringLength)),
+                        .on_press(Msg::GatherMetric(Metric::AverageContentStringLength))
+                        .padding(3),
                     text("Average ContentString Length:"),
                     text(format!(
                         "[{}]",
@@ -166,13 +234,50 @@ impl LogPane {
                 .spacing(3)
                 .align_items(Alignment::Center);
 
-                let content = column![content_string_length]
+                let url_domain_count =
+                    if let MetricValue::UrlMap(ref map) = app_view.metrics.url_occurances {
+                        let tally = map.tally();
+                        text(format!(
+                            "[Count: {}, None: {}, Err: {}]",
+                            tally.domains, tally.no_domain, tally.parse_issues,
+                        ))
+                    } else {
+                        text("[None]")
+                    };
+
+                let url_domain_row = row![
+                    button("Gather")
+                        .on_press(Msg::GatherMetric(Metric::UrlOccurances))
+                        .padding(3),
+                    text("Url Domain Count:"),
+                    url_domain_count,
+                ]
+                .spacing(3)
+                .align_items(Alignment::Center);
+
+                let content = column![content_string_length, url_domain_row]
                     .height(Length::Fill)
                     .width(Length::Shrink)
-                    .align_items(Alignment::Start);
+                    .align_items(Alignment::Start)
+                    .spacing(3);
+
+                let content = if let MetricValue::UrlMap(ref map) = app_view.metrics.url_occurances
+                {
+                    let mut occurances = map.occurance_count.iter().collect::<Vec<_>>();
+                    occurances.sort_by_key(|(_, v)| *v);
+
+                    occurances
+                        .into_iter()
+                        .rev()
+                        .fold(content, |content, (k, v)| {
+                            content.push(text(format!("{k}: {v}")))
+                        })
+                } else {
+                    content
+                };
 
                 Content::new(
-                    column![content]
+                    column![scrollable(content)]
                         .spacing(3)
                         .padding(3)
                         .width(Length::Fill)
