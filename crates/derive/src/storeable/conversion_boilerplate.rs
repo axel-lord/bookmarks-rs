24250e32d3e428b950d7d5129c86417db10495a7
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2023-01-16 22:14:41 +0100

    Revised some names

--- crates/derive/src/storeable/conversion_boilerplate.rs
+++ crates/derive/src/storeable/conversion_boilerplate.rs
@@ -12,14 +12,21 @@ pub fn conversion_boilerplate(name: &syn::Ident) -> proc_macro2::TokenStream {
             type Error = bookmark_storage::ParseErr;
             fn try_from(value: String) -> Result<Self, Self::Error> {
                 use bookmark_storage::Storeable;
-                Self::with_string(value, None)
+                Self::from_string(value, None)
             }
         }
         impl std::convert::TryFrom<&str> for #name {
             type Error = bookmark_storage::ParseErr;
             fn try_from(value: &str) -> Result<Self, Self::Error> {
                 use bookmark_storage::Storeable;
-                Self::with_string(value, None)
+                Self::from_string(value, None)
+            }
+        }
+        impl std::convert::TryFrom<bookmark_storage::ContentString> for #name {
+            type Error = bookmark_storage::ParseErr;
+            fn try_from(value: bookmark_storage::ContentString) -> Result<Self, Self::Error> {
+                use bookmark_storage::Storeable;
+                Self::from_content_string(value, None)
             }
         }
 
